7516dce8d4ba76b4cd60ff29b6bf86e105157bd1
luca <lm@roadtriptechnologies.com>
2026-03-05 09:45:22 +0000

    Fix add-on schema compatibility (scanner_positions_json string)

--- ble-room-map/run.sh
+++ ble-room-map/run.sh
@@ -6,7 +6,7 @@ export MQTT_PORT="$(bashio::config 'mqtt_port')"
 export MQTT_USER="$(bashio::config 'mqtt_user')"
 export MQTT_PASS="$(bashio::config 'mqtt_pass')"
 export TOPIC_PREFIX="$(bashio::config 'topic_prefix')"
-export SCANNER_POSITIONS="$(bashio::config 'scanner_positions')"
+export SCANNER_POSITIONS="$(bashio::config 'scanner_positions_json')"
 export TX_POWER="$(bashio::config 'tx_power')"
 export N_FACTOR="$(bashio::config 'n_factor')"
 
